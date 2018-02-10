fdef7206192815a1397fe0a68c74f06c4fbfa015
Aryaman Sharda <aryaman@bixbyapps.com>
2018-02-09 17:06:17 -0800

    Initial Commit

---
+++ Porsche/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Porsche
+//
+//  Created by Aryaman Sharda on 2/9/18.
+//  Copyright © 2018 Aryaman Sharda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Porsche/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Porsche
+//
+//  Created by Aryaman Sharda on 2/9/18.
+//  Copyright © 2018 Aryaman Sharda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
